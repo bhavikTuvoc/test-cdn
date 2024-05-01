4b5c3c44aa0a000cea1bf5b43972d412a13e33a4
bhavik-tuvoc <bhavik@tuvoc.com>
2024-05-01 11:07:38 +0530

    ds

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     // <div className="App app-container" style={{ display: "none" }}>
     <div className="App app-container" style={{ display: "block" }}>
-      <div className="bgBlur popUpWrapper">
+      <div className="bgBlur popUpWrapper bgRed">
         <PopUp />
       </div>
     </div>
